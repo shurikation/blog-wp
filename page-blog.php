ceee04f17fcf68bb6ddb72f72e218528ab678ffa
shurikation <shurikation@gmail.com>
2020-10-03 16:36:44 +0400

    fix: changed output of posts through a loop

--- page-blog.php
+++ page-blog.php
@@ -6,39 +6,49 @@
 <?php
     get_header();
 ?>
-<section class="blog">
+<main class="blog">
     <div class="container">
-        <h1 class="main-title">&lt;Блог&#47;&#62;</h1>
-        <div class="blog__wrapper">
+        <a target="_blank" rel="noopener" href="<?php echo get_permalink();?>blog">
+            <h2 class="main-title">&lt;Блог&#47;&#62;</h2>
+        </a>
+        <div class="blog__content">
+            <!--ALL POSTS IN LOOP-->
             <?php
             $posts = get_posts(array(
-                'numberposts' => 0, //чтобы вывести ВСЕ посты
-                'category_name' => 'blog', //выводим только рубрику projects
+                'numberposts' => 0, //чтобы вывести последние 3 поста
+                'category_name' => 'blog', //выводим только рубрику blog
                 'orderby' => 'date',
                 'order' => 'DESC', //DESC - прямой порядок, ASC - обратный порядок
                 'post_type' => 'post',
                 'suppress_filters' => true, // подавление работы фильтров изменения SQL запроса
             ));
-
             foreach ($posts as $post) {
                 setup_postdata($post);
                 ?>
-                <div class="blog__post post"><img class="post__image"
-                                                  src="<?php the_field('blog_img');?>"
-                                                  alt="picture">
-                    <div class="post__info">
-                        <h2 class="post__title"><?php the_title();?></h2>
-                        <p class="main-text"><?php the_field('blog_text');?></p>
-                        <a class="post__button button" target="_blank" rel="noopener"
-                           href="<?php echo get_permalink();?>">Далее...</a>
+                <div class="blog__single-post single-post">
+                    <img class="single-post__image" src="<?php the_field('blog_img');?>"
+                         alt="<?php the_field('blog_alt');?>">
+                    <div class="single-post__info">
+                        <h3 class="single-post__title"><?php the_title();?></h3>
+                        <p class="main-text single-post__text"><?php the_field('blog_text');?></p>
+                        <a class="single-post__button button" target="_blank" rel="noopener"
+                           href="<?php echo get_permalink();?>">Далее</a>
                     </div>
                 </div>
                 <?php
             }
             wp_reset_postdata();
             ?>
+            <!-- THE END OF ALL POSTS-->
+            <!-- BUTTON-LINK TO ALL POSTS-->
+            <div class="blog__button-wrapper">
+                <a class="blog__button button" target="_blank" rel="noopener"
+                   href="<?php echo get_permalink();?>blog">Все публикации...</a>
+            </div>
+            <!-- BUTTON ENDS-->
         </div>
-</section>
+    </div>
+</main>
 <?php
     get_footer();
 ?>
