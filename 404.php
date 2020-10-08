c61a121c079923006ee409d2159881eec68d2149
shurikation <shurikation@gmail.com>
2020-10-08 19:07:18 +0400

    feat: add 404 page

---
+++ 404.php
@@ -0,0 +1,17 @@
+<main id="primary" class="site-main">
+    <section class="error-404 not-found">
+        <header class="page-header">
+            <h1 class="page-title"><?php esc_html_e( 'Oops! That page can&rsquo;t be found.', 'blog-theme' ); ?></h1>
+        </header><!-- .page-header -->
+
+        <div class="page-content">
+            <p><?php esc_html_e( 'It looks like nothing was found at this location. Maybe try one of the links below or a search?', 'blog-theme' ); ?></p>
+
+			<?php
+			the_widget( 'WP_Widget_Recent_Posts' );
+			?>
+
+        </div><!-- .page-content -->
+    </section><!-- .error-404 -->
+</main><!-- #main -->
+
