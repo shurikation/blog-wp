e0d15c8bb7143a1d40985eb0b822c785f24d3800
shurikation <shurikation@gmail.com>
2020-10-09 17:31:43 +0400

    fix: deleted unused functions and refactored code

--- functions.php
+++ functions.php
@@ -11,36 +11,23 @@ function blog_scripts() {
     wp_enqueue_script('blog-scripts', get_template_directory_uri().'/app.js', array(), null, true);
 }
 
-
-
-//add_theme_support( 'custom-logo' );
-//add_theme_support( 'post-thumbnails' );
 add_theme_support( 'menus' );
-//add_theme_support('custom-background');
 
-add_filter('nav_menu_link_attributes', 'filter_nav_menu_link_attributes', 10, 3); //3 аргумента, 10 - приоритет станартный
+add_filter('nav_menu_link_attributes', 'filter_nav_menu_link_attributes', 10, 3);
 
-//Этот хук отловит все аттрибуты у наших динамических ссылок
-function filter_nav_menu_link_attributes($atts, $item, $args)
-{
-    if ($args->menu === 'Main') //если это главное меню
-
-    {
-        $atts['class'] = 'menu__link'; //таким образом добавляем класс в аттрибу ссылок
+function filter_nav_menu_link_attributes($atts, $item, $args){
+    if ($args->menu === 'Main') {
+        $atts['class'] = 'menu__link';
     }
     return $atts;
 }
-//atts - html-аттрибуты ссылок
-//item - каждая отдельная ссылка
-//args - все аргументы меню (все что есть в wp_nav_menu())
 
-//Этот хук отловит все аттрибуты у наших динамических ли-шек
 add_filter( 'nav_menu_css_class', 'filter_nav_menu_list_attributes', 10, 4 );
+
 function filter_nav_menu_list_attributes( $atts, $item, $args ){
     if($args->menu === 'Main') {
         $atts['class'] = 'menu__item';
 
-        //Если я нахожусь в категории БЛОГ, и в цикле мы выводим меню с ID который соотвествует блогу,то
         if( $item->ID === 56 && (in_category('blog'))) {
             $atts['class'] .= ' menu__item--active';
         }
@@ -49,15 +36,11 @@ function filter_nav_menu_list_attributes( $atts, $item, $args ){
             $atts['class'] .= ' menu__item--active';
         }
 
-        //чтобы подсвечивать активную ссылку
-        if ($item->current) //если текущая страница, на которой находимся, является активной, то берем ссылку и назначаем ей новый класс
-        {
+        if ($item->current) {
             $atts['class'] .= ' menu__item--active';
         }
     }
     return $atts;
 }
-
-
 ?>
 
