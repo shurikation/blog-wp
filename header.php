31362a2842d0120ab42d3628f4f8474ab0a3301f
shurikation <shurikation@gmail.com>
2020-10-14 17:22:00 +0400

    feat: added dynamic titles for each page

--- header.php
+++ header.php
@@ -1,12 +1,38 @@
 <!doctype html>
 <html lang="ru">
 <head>
+    <!-- Yandex.Metrika counter -->
+    <script type="text/javascript" >
+      (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+        m[i].l=1*new Date();k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+      (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+
+      ym(68162149, "init", {
+        clickmap:true,
+        trackLinks:true,
+        accurateTrackBounce:true
+      });
+    </script>
+    <noscript><div><img src="https://mc.yandex.ru/watch/68162149" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
+    <!-- /Yandex.Metrika counter -->
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width,initial-scale=1">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Блог веб-разработчика | Александр Попов</title>
+    <title>
+        <?php $ID = get_the_ID();
+        $full_title = 'Блог веб-разработчика | Александр Попов';
+        $short_title = ' | Блог веб-разработчика';
+        if($ID == 2) {
+            echo $full_title;
+        } else if( $ID == 51 || $ID == 53) {
+            echo get_the_title() . $short_title;
+        } else {
+            echo get_the_title($ID);
+        }
+        ?>
+    </title>
     <?php
-        wp_head();
+    wp_head();
     ?>
 </head>
 <body>
